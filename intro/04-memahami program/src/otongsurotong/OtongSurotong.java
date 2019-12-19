a7357da72b501f64582c83e3eee766cda8b9b521
regamiftah1234 <regamiftah10@gmail.com>
2019-12-19 18:57:16 +0700

    pertemuan_13-14

---
+++ intro/04-memahami program/src/otongsurotong/OtongSurotong.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otongsurotong;
+
+/**
+ *
+ * @author Rega
+ * Nama     :   Rega miftachudin
+ * NIM      :   A2.1900194
+ * Kelas    :   TI-IA
+ * Pertemuan 13-14
+ */
+public class OtongSurotong {
+    public static void main(String[] args){
+
+		System.out.print("Nama ku adalah Otong, teman si Ucup");
+
+	}
+    
+}
